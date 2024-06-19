f6c6bb915737797005b2648c685f449965f16426
igoramf <igoraaf1@gmail.com>
2024-06-19 10:48:06 -0300

    chore: reorganizing js

---
+++ app/public/plot_data.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    createCharts();
+    loadDataFromLocalStorage();
+
+    // const socket = io('https://plot-server-vyls.onrender.com');
+    const socket = io('http://localhost:3000');
+      socket.on('connect', () => {
+        const roomId = get_plot_id(); 
+        socket.emit('join_room', roomId);
+    });
+
+    socket.on('update_chart', (data) => {
+      const { train_acc, val_acc, loss, val_loss, max_epochs, id } = data;
+
+      updateLabels(max_epochs);
+
+      uc(train_acc, val_acc, loss, val_loss);
+ 
+      const epoch = window.charts[0].data.datasets[0].data.length;
+      if (epoch === max_epochs + 1) {
+          console.log('Training finished');
+          emitPlots(socket);
+          ClearData()
+      }
+    });
+};
