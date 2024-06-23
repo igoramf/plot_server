5c882e50a5a3127a56dac81b1d9f67eaa76e8183
igoramf <igoraaf1@gmail.com>
2024-06-23 12:32:31 -0300

    fix: prod url

--- data.py
+++ data.py
@@ -8,7 +8,9 @@ from PlotData import *
 plot_data_client = PlotData()
 plot_data_client.connect()
 
-while True:
+cont = 0
+max_epochs = 10
+while max_epochs > cont:
     train_acc = random.random()
     val_acc = random.random()
     loss = random.random()
@@ -16,8 +18,9 @@ while True:
     
     
     
-    plot_data_client.call(train_acc, val_acc, loss, train_loss, 10)
+    plot_data_client.call(train_acc, val_acc, loss, train_loss, max_epochs)
     print(plot_data_client.url)
+    cont += 1
      
     # print(f'Dados enviados: train_acc={train_acc}, val_acc={val_acc}, loss={loss}, val_loss={train_loss}, max_epochs=10')
 
